cd51764c4e53af8cfb79f3baaf70762b0ac82265
Carlos Pontón <carlos.ponton2@gmail.com>
2018-10-31 21:41:29 -0500

    fight with heroku

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from "path";
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'dist'));
   await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
